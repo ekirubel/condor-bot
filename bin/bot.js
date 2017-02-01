2cb7b529372593e04b72dff7f6ce9b9c5ffb3813
Ermias Kirubel <ekirubel@gmail.com>
2017-01-31 19:55:51 -0800

    Adding karma functionality (just ++ and -- for now), using mongodb as storage

--- bin/bot.js
+++ bin/bot.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var CondorBot = require('../lib/condorbot');
+var KarmaBot = require('../lib/karmabot');
+
 
 var token = process.env.BOT_API_KEY;
 var name = process.env.BOT_NAME;
+var db_url = process.env.MONGODB_URI;
 
-var condorbot = new CondorBot({
+var karmabot = new KarmaBot({
     token: token,
-    name: name
+    name: name,
+    db_url: db_url
 });
